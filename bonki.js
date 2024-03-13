7cc5bab60d4cc04c443fbc91a8011d73b1a7cdf3
MarvMuon <162844746+MarvMuon@users.noreply.github.com>
2024-03-13 22:17:59 +0100

    Add files via upload

---
+++ bonki.js
@@ -0,0 +1,17 @@
+{
+  "name": "TesttoDel",
+  "symbol": "TDEL",
+  "description": "Random",
+  "image": "https://github.com/MarvMuon/bonki/blob/main/logo.png?raw=true",
+  "animation_url": "https://drive.google.com/file/d/1L87FDVPTC4gsqvz0-3-NwVFgYnhFbt7l/view?usp=drive_link",
+  "external_url": "https://umeus.com",
+  "attributes": [
+    {"color": "gold"},
+    {"supply": ""}
+  ],
+  "properties": {
+    "files": [
+      {"uri": "https://www.gutenberg.org/cache/epub/50294/pg50294-images.html", "type": "application/html"}
+    ],
+      }
+}
